2214f8e359fcbe82218aeedc71d992e065faa4ed
mikeyxgcg <45147548+mikeyxgcg@users.noreply.github.com>
2019-01-03 20:04:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -75,5 +75,32 @@ password : **${random2}**
 message.channel.send("**تم الارسال الحساب في الخاص | ☑ **")
 }});
 
+client.on('message', message => {
+    var prefix = "-";
+   
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'bc') {
+        if (!args[1]) {
+    message.channel.send("**اكتب شي بعد الكوماند**");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                var bc = new Discord.RichEmbed()
+                .addField('» السيرفر :', `${message.guild.name}`)
+                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+                .addField(' » الرسالة : ', args)
+                .setColor('#ff0000')
+                // m.send(`[${m}]`);
+                m.send(`${m}`,{embed: bc});
+            });
+        }
+        } else {
+            return;
+        }
+    });
 
 client.login(process.env.BOT_TOKEN);
