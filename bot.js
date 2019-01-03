1e84f3951bd1cac3bcea0065d13e9a42991b13b9
mikeyxgcg <45147548+mikeyxgcg@users.noreply.github.com>
2019-01-03 23:43:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -98,27 +98,6 @@ client.on('message', message => {
                 m.send(`${m}`,{embed: bc}   
 }); 
               
-       client.on('message', msg => {
-  if (msg.content === '-help') {
-    msg.author.send(`
-● ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ● 
-                        
--server 『معلومات عن السيرفر』                      
-
--brodcast 『 ارسال رساله جمعايه في الخاص 』
-
--cl『 قفل الشات 』
-
--op『فتح الشات 』
-
--clear 『 لمسح الشات 』
-
--say 『لي يكرر الكلام الذي تقوله』  تم ايقافها
-
-● ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ●  
-    
-    `);
-  }
-});
+     
       
 client.login(process.env.BOT_TOKEN);
