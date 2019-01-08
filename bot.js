268c176e26451c8bd2c896ed2b1315fe01743ca1
mikeyxgcg <45147548+mikeyxgcg@users.noreply.github.com>
2019-01-08 17:39:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -540,30 +540,6 @@ client.on('message', msg => {
   }
 });
 
-client.on('message', msg => {
-  if (msg.author.bot) return;
-  if (!msg.content.startsWith(prefix)) return;
-  let command = msg.content.split(" ")[0];
-  command = command.slice(prefix.length);
-  let args = msg.content.split(" ").slice(1);
- 
-    if(command === "-clear") {
-        const emoji = client.emojis.find("name", "wastebasket")
-    let textxt = args.slice(0).join("");
-    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
-    if (textxt == "") {
-        msg.delete().then
-    msg.channel.send("***```Supply A Number ًں‘Œ```***").then(m => m.delete(3000));
-} else {
-    msg.delete().then
-    msg.delete().then
-    msg.channel.bulkDelete(textxt);
-        msg.channel.send("```Cleard: " + textxt + "\n Messages```").then(m => m.delete(3000));
-        }    
-    }
-}
-});
-
 client.on('message', message => {
 var prefix = "-";
        if(message.content === prefix + "cl") {
@@ -590,6 +566,29 @@ var prefix = "-";
               });
     }
        
+});
+
+client.on("message", message => {
+    var prefix = "-";
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **ليس لديك صلاحيات**');
+        var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done | تــم",
+        color: 0x00a300,
+        description: ":white_check_mark: | Delete " + args[1] + " Message!",
+        footer: {
+          text: "اسم بوتك هنا"
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
+
+     
 });
 
 client.login(process.env.BOT_TOKEN);
