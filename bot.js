4f372806afd3412044072f67ef1293af22c8f3a1
mikeyxgcg <45147548+mikeyxgcg@users.noreply.github.com>
2019-01-08 13:10:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -135,13 +135,6 @@ client.on('message', message => {
     }
 });
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
 client.on('message', msg => {
   if (msg.content === 'hello') {
     msg.reply('**Welcome to HythoServer**');
