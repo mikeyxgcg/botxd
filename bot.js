285073659c5d615eccdfcc0246130c0454ed6f22
mikeyxgcg <45147548+mikeyxgcg@users.noreply.github.com>
2019-01-09 15:09:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -519,7 +519,7 @@ var prefix = "-"
   command = command.slice(prefix.length);
   let args = msg.content.split(" ").slice(1);
  
-    if(command === "-clear") {
+    if(command === "clear") {
         const emoji = client.emojis.find("name", "wastebasket")
     let textxt = args.slice(0).join("");
     if(msg.member.hasPermission("MANAGE_MESSAGES")) {
