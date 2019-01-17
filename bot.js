9c262720f696d69b73b920518167c5ac0c354e69
mikeyxgcg <45147548+mikeyxgcg@users.noreply.github.com>
2019-01-17 23:53:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,18 +76,39 @@ password : **${random2}**
 message.channel.send("**تم الارسال الحساب في الخاص | ☑ **")
 }});
 
-client.on("message", message => {
-
-            if (message.content.startsWith(prefix + "bc")) {  ///الامر
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` **: عدد الاعضاء المستلمين**`); 
- message.delete(); 
-};     
+client.on('message', message => {
+	var prefix = "-"
+  if (message.guild) {
+ let embed = new Discord.RichEmbed()
+  let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc') {
+  if (!args[1]) {
+message.channel.send("Usage: -bc [الرسالة]");
+return;
+}
+      message.guild.members.forEach(m => {
+ if(!message.member.hasPermission('ADMINISTRATOR')) return;
+          var bc = new Discord.RichEmbed()
+          .setAuthor(message.author.username, message.author.avatarURL)
+          .addField(' Server', ${message.guild.name}, true)
+          .addField(' Sender ', ${message.author.username}!${message.author.discriminator}, true)
+          .addField(' Message ', args)
+          .setThumbnail(message.guild.iconURL)
+          .setColor('RANDOM')
+          m.send(${m},{embed: bc});
+      });
+      const unknown = new Discord.RichEmbed()
+      .setAuthor(message.author.username, message.author.avatarURL)
+      .setTitle(':white_check_mark:| the messege is loading ')
+      .addBlankField(true)
+      .addField(':hotsprings:| i got sended to  ', message.guild.memberCount , true)
+      .addField(':pencil:| the message ', args)
+      .setColor('RANDOM')
+      message.channel.sendEmbed(embed);
+  }
+  } else {
+      return;
+  }
 });
 
 client.on('message', msg => {
