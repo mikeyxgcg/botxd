c7ede6d2bbf241ed837f35406b5460bf3bfb05ed
mikeyxgcg <45147548+mikeyxgcg@users.noreply.github.com>
2019-01-17 23:36:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -74,38 +74,21 @@ password : **${random2}**
 });
 
 message.channel.send("**تم الارسال الحساب في الخاص | ☑ **")
-}}); 
+}});
 
-     
- client.on('message', message => {
-    var prefix = "-";
-   
-        if (message.author.id === client.user.id) return;
-	  if (!message.member.hasPermission("ADMINISTRATOR"))
-        if (message.guild) {
-       let embed = new Discord.RichEmbed()
-        let args = message.content.split(' ').slice(1).join(' ');
-    if(message.content.split(' ')[0] == prefix + 'bc') {
-        if (!args[1]) {
-    message.channel.send("**اكتب شي بعد الكوماند**");
-    return;
-    }
-            message.guild.members.forEach(m => {
-       if(!message.member.hasPermission('ADMINISTRATOR')) return;
-                var bc = new Discord.RichEmbed()
-                .addField('» السيرفر :', `${message.guild.name}`)
-                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
-                .addField(' » الرسالة : ', args)
-                .setColor('#ff0000')
-                // m.send(`[${m}]`);
-                m.send(`${m}`,{embed: bc});
-            });
-        }
-        } else {
-            return;
-        }
-    });
+client.on("message", message => {
 
+            if (message.content.startsWith(prefix + "bc")) {  ///الامر
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` **: عدد الاعضاء المستلمين**`); 
+ message.delete(); 
+};     
+});
 
 client.on('message', msg => {
   if (msg.content === '-help') {
